58f8e4a5a7c201f1e3c493f59ad21a4b809dcee2
Basix <basix@basix.tech>
2025-11-01 18:46:45 +0900

    fix: discord token

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,15 @@ import { Client, Events, GatewayIntentBits } from "discord.js";
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on(Events.ClientReady, (readyClient) => {
-	console.log(`Logged in as ${readyClient.user.tag}!`);
+  console.log(`Logged in as ${readyClient.user.tag}!`);
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
-	if (!interaction.isChatInputCommand()) return;
+  if (!interaction.isChatInputCommand()) return;
 
-	if (interaction.commandName === "ping") {
-		await interaction.reply("Pong!");
-	}
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
 });
 
-client.login();
+client.login(process.env.DISCORD_TOKEN);
